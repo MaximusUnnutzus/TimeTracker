cc6d846578274ada3bce0c695d635e0700123de3
MaximusUnnutzus <wolfsmax@students.zhaw.ch>
2022-10-24 21:40:23 +0200

    closes#1

---
+++ src/test/java/ch/zhaw/timetracker/TimetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.timetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
